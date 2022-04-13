c738480e68a4676538206b4257a5f0d1a3f1be13
happymarcker <99589643+happymarcker@users.noreply.github.com>
2022-04-13 06:04:31 -0400

    Secouer pour obtenir une valeur de 1 à 6

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
-basic.forever(function () {
-	
+input.onGesture(Gesture.Shake, function () {
+    Dé = randint(1, 6)
+    basic.showNumber(Dé)
+    basic.pause(1000)
+    basic.clearScreen()
 })
+let Dé = 0
+Dé = 0
